0138362f63288448bd9feaa01931b4de9e7509cb
N1kken <w.bak95@gmail.com>
2018-02-21 22:48:42 +0100

    PythonSQL

--- PythonSQL/wypozyczalnia.py
+++ PythonSQL/wypozyczalnia.py
@@ -13,6 +13,7 @@ def create_film_table():
 			) """)
 		db.commit() #zmiany
 #create_film_table()
+
 def insert_records(values):
 	with sqlite3.connect('wypozyczalnia.db') as db:
 		c=db.cursor()
@@ -21,8 +22,12 @@ def insert_records(values):
 		c.execute(sql,values)
 		db.commit()
 		
-film1=("Minionki","Bajka", 2014)
-insert_records(film1)
+def delete_record(tytul):
+	with sqlite3.connect('wypozyczalnia.db') as db:
+		c=db.cursor()
+		sql="DELETE FROM Film WHERE tytul = ?"
+		c.execute(sql,(tytul,))
+		db.commit()
 
 def select_all_films():
 	with sqlite3.connect('wypozyczalnia.db') as db:
@@ -39,7 +44,6 @@ def select_one_film():
 		c.execute(sql)
 		wynik=c.fetchone()
 		print(wynik)
-select_all_films()
 
 def select_genre(typ):
 	with sqlite3.connect("wypozyczalnia.db") as db:
@@ -49,6 +53,46 @@ def select_genre(typ):
 		result = cursor.fetchall()
 		return result
 
-wybor = input("Podaj typ: ")
-films = select_genre(wybor)
-print(films)
+def select_year(rok):
+	with sqlite3.connect("wypozyczalnia.db") as db:
+		cursor = db.cursor()
+		sql = "Select * from Film where rok = ?"
+		cursor.execute(sql,(rok,))
+		result = cursor.fetchall()
+		return result
+		
+#wybor = input("Podaj typ: ")
+#films = select_genre(wybor)
+#print(films)
+
+while 1:
+	print ("WYPOZYCZALNIA FILMOW")
+	print ("====================")
+	print ("1 - Dodaj film")
+	print ("2 - Usun film")
+	print ("3 - Pokaz wszystkie filmy")
+	print ("4 - Znajdz filmy w zadanych latach")
+	print ("5 - Wyjscie")
+	x = int(input())
+	if x == 1:
+		tytul = input ("Podaj tytul: ")
+		typ = input ("Podaj typ: ")
+		print ("Podaj rok: ", end='')
+		rok = int(input())
+		nastepny_film = (tytul,typ,rok)
+		insert_records(nastepny_film)
+	elif x == 2:
+		tytul = input ("Podaj tytul filmu ktory chcesz usunac: ")
+		delete_record(tytul)
+	elif x == 3:
+		select_all_films()
+	elif x == 4:
+		print ("Podaj rok")
+		rok = int(input())
+		filmy = select_year(rok)
+		print (filmy)
+	elif x == 5:
+		break
+	else:
+		print ("Podaj prawidlowa liczbe!")
+		
